2fcbaf5be054462e897cb2668ccba0ea427c7ec6
ammar10 <ammar.tariq@hotmail.com>
2018-03-28 18:25:52 +0500

    working but incomplete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  i = 0;
+  data = []
+  inputVal: string = "";
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    if(localStorage.getItem('masterkey')){
+      this.i =  parseInt(localStorage.getItem('masterkey'))
+      this.getData()
+    }
 
-  constructor(public navCtrl: NavController) {
 
+   
+    
   }
+
+setData(inputValue){
+  if(inputValue != null){
+      localStorage.setItem('Datakey'+this.i.toString(),inputValue)
+      this.i++
+      localStorage.setItem('masterkey',this.i.toString())
+      this.data.unshift(inputValue) 
+      this.inputVal = ""
+    }else{
+      console.log('null values are not accepted')
+    }
+}
+
+
+emptyStorage(){
+  localStorage.clear()
+  this.data =[]
+  this.i = 0
+}
+
+getData(){
+    for (var j = localStorage.length; j>0; j--) {
+        let item = localStorage.getItem(localStorage.key(j));
+        this.data[j] = item
+        console.log(item)
+      }
+   console.log(this.data)
+}
+
+
+
 
 }
