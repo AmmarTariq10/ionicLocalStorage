0ed8437a5dee847fe6b9341dfa944ff8150c0590
ammar10 <ammar.tariq@hotmail.com>
2018-03-29 16:45:20 +0500

    asd

--- src/pages/appointment/appointment.ts
+++ src/pages/appointment/appointment.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 
 
@@ -8,40 +8,96 @@ import { IonicPage, NavParams, ViewController } from 'ionic-angular';
   selector: 'page-appointment',
   templateUrl: 'appointment.html',
 })
-export class AppointmentPage {
-DateTime:any;
 
-  constructor(public view: ViewController, public navParams: NavParams) {
-  }
+export class AppointmentPage {
+i=0
+appointment={
+  'title':'',
+  'date':'',
+  'time':''
+}
+
+formDate=''
+formTime=''
+formTitle=''
+
+currentDateTime = {
+  'cTime':'',
+  'cDate':''
+}
+
+
+constructor(public view: ViewController, public navParams: NavParams, public alert: AlertController) {
+  this.getCurrentDate()
+}
+
+
+getCurrentDate(){
+let cd = new Date();
+console.log(cd.toISOString())
+this.currentDateTime.cDate = cd.toISOString().substring(0,10)
+console.log(this.currentDateTime.cDate)
+this.currentDateTime.cTime = cd.toISOString().substring(11,16)
+console.log(this.currentDateTime.cTime)
+}
+
+
 
 
   dismiss(){
-    this.view.dismiss()
+        this.view.dismiss()
+  }
+
+  
+  addAppointment(){
+
+      this.appointment.title=this.formTitle
+      this.appointment.date=this.formDate
+      this.appointment.time=this.formTime
+      this.confirmation()
+    }
+
+
+    confirmation(){
+
+      let alert = this.alert.create({
+        title: this.appointment.title,
+        message: 'Are you sure want to add an appointment for ' + this.appointment.date + ' time : ' + this.appointment.time,
+        buttons: [
+          {
+            text: 'Save',
+            handler: ()=>{
+              this.saveAppointment()
+              this.formDate=''
+              this.formTime=''
+              this.formTitle=''
+              console.log('appointment saved')  
+              
+            }
+          },
+          {
+            text: 'Cancel',
+            handler: ()=>{
+                this.formDate=''
+                this.formTime=''
+                this.formTitle=''
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
+
+  saveAppointment(){
+
+    if(this.formDate != '' && this.formTime != '' && this.formTitle != ''){
+       localStorage.setItem(this.appointment.title,JSON.stringify(this.appointment)) 
+       this.dismiss()
+      }else{
+        console.log('null values are not accepted')
+      }
   }
 
 
-  // showPicker(){
-  //   this.dp.show({
-  //     date: new Date(),
-  //     mode: 'date',
-  //     androidTheme: this.dp.ANDROID_THEMES.THEME_HOLO_DARK
-  //   }).then(
-  //     date => console.log('Got date: ', date),
-  //     err => console.log('Error occurred while getting date: ', err)
-  //   );
-  // }
-
-  filter(){
-
-    console.log(this.DateTime)
-  }
-  // filter2(){
-  //   console.log(this.someVar2)
-  // }
-
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AppointmentPage');
-  }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angu
 
 
 @IonicPage()
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -12,51 +13,48 @@ export class HomePage {
   i = 0;
   data = []
   inputVal: string = "";
+  message=''
   constructor(public navCtrl: NavController, public navParams: NavParams, public modal: ModalController) {
-    if(localStorage.getItem('masterkey')){
-      this.i =  parseInt(localStorage.getItem('masterkey'))
+    if(localStorage.length == 0){
+        this.message = 'You currently have no appointments'
+    }else{
       this.getData()
     }
-
-
-   
-    
-  }
-
-setData(inputValue){
-  if(inputValue != null){
-      localStorage.setItem('Datakey'+this.i.toString(),inputValue)
-      this.i++
-      localStorage.setItem('masterkey',this.i.toString())
-      this.data.unshift(inputValue) 
-      this.inputVal = ""
-    }else{
-      console.log('null values are not accepted')
-    }
 }
+
 
 
 addAppointment(){
   console.log('added')
   let AppointmentPageModal = this.modal.create('AppointmentPage')
-  AppointmentPageModal.present()
+  AppointmentPageModal.onDidDismiss(() => {
+    this.getData()
+    console.log('modal dismissed');
+  });
 
-}
+  AppointmentPageModal.present()
+  }
+
+
 
 emptyStorage(){
   localStorage.clear()
   this.data =[]
-  this.i = 0
+  this.message = ""
 }
 
+
+
 getData(){
-    for (var j = localStorage.length; j>0; j--) {
-        let item = localStorage.getItem(localStorage.key(j));
-        this.data[j] = item
-        console.log(item)
-      }
-   console.log(this.data)
+
+  for (var j = localStorage.length-1; j>=0; j--) {
+      let item = localStorage.getItem(localStorage.key(j));
+      this.data[j] = JSON.parse(item)
+      console.log(this.data[j])
+    }
+ console.log(this.data)
 }
+
 
 
 
