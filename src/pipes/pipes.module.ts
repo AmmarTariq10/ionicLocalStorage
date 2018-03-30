f5850311a8a0d25b37e78cb900eb3a53b027b2f0
ammar10 <ammar.tariq@hotmail.com>
2018-03-30 18:26:16 +0500

    test

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SortByDateTimePipe } from './sort-by-date-time/sort-by-date-time';
 import { IntDtPipe } from './int-dt/int-dt';
-import { ArrangePipe } from './arrange/arrange';
+
 import { KeyCreatorPipe } from './key-creator/key-creator';
 @NgModule({
 	declarations: [SortByDateTimePipe,
     IntDtPipe,
-    ArrangePipe,
-    KeyCreatorPipe,
-    KeyCreatorPipe],
+    KeyCreatorPipe,],
 	imports: [],
 	exports: [SortByDateTimePipe,
     IntDtPipe,
-    ArrangePipe,
-    KeyCreatorPipe,
     KeyCreatorPipe]
 })
 export class PipesModule {}
